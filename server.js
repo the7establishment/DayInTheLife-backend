7f66f41b64bfa2d690b801cabebdf2ddc612b378
Matthew Chuong <mstchuong@yahoo.com>
2020-10-16 23:50:19 -0400

    :heavy_plus_sign: Add nodemon
    -Clean up server.js

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 var express = require('express')
-var bodyParser = require('body-parser')
 var server = express()
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 4201;
-// var mongoClient = require('mongodb').MongoClient
-var connectionString = ''
-var mongoose = require('mongoose')
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:" + process.env.Mongo_Admin_PW + "@dayinthelife-dev.ozz3z.mongodb.net/testdb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
-//routes
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(bodyParser.json());
-
-//connect to the Database
-// mongoose.connect(connectionString)
 
 server.listen(port, function(){
   console.log('Server is running my server on PORT: ' + port)
