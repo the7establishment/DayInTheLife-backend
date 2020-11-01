dfffbcd429f152726d3f35414d438cdc86edc776
Matthew Chuong <mstchuong@yahoo.com>
2020-11-01 11:31:04 -0500

    Add getById for user endpoints

--- api/user/user.route.js
+++ api/user/user.route.js
@@ -4,6 +4,7 @@ var router = express.Router()
 const user_controller = require('./user.controller')
 
 router.get('/', user_controller.get)
+router.get('/:userId', user_controller.getById)
 router.post('/', user_controller.post)
 
 module.exports = router
